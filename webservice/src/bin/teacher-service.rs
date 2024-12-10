df6d4d4cd27f04d06ea9d1dfefbe35162dddda0c
cjb <75364055@qq.com>
2024-12-10 22:33:24 +0800

    feat:use sqlx

--- webservice/src/bin/teacher-service.rs
+++ webservice/src/bin/teacher-service.rs
@@ -1,6 +1,8 @@
-use std::io;
+use std::{env, io};
 use std::sync::Mutex;
 use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use crate::routers::{course_routes, general_routes};
 use crate::state::AppState;
 
@@ -15,10 +17,20 @@ mod models;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL")
+        .expect("DATABASE_URL is not set in .env file");
+
+    let db_pool = PgPoolOptions::new()
+        .connect(&database_url)
+        .await
+        .unwrap();
+
     let shared_data = web::Data::new(AppState {
         health_check_response: "i'm ok".to_string(),
         visit_count: Mutex::new(0),
-        courses: Mutex::new(vec![]),
+        db: db_pool,
     });
 
     let app = move || {
--- webservice/src/state.rs
+++ webservice/src/state.rs
@@ -1,9 +1,10 @@
 use std::sync::Mutex;
-use crate::models::Course;
+use sqlx::postgres::PgPool;
 
 // #[derive(FromRequest)]
 pub struct AppState {
     pub health_check_response: String,
     pub visit_count: Mutex<u32>,
-    pub courses: Mutex<Vec<Course>>,
+    // pub courses: Mutex<Vec<Course>>,
+    pub db: PgPool,
 }
