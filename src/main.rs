ad5fdc32591acc44182965ba532d3fdfc269eab4
cjb <75364055@qq.com>
2024-12-10 21:06:26 +0800

    actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", generate_uuid());
+}
+
+fn generate_uuid() -> usize {
+    // Utc::now().timestamp()
+    Utc::now().timestamp_millis().try_into().unwrap()
 }
--- webservice/src/main.rs
+++ webservice/src/main.rs
@@ -1,3 +1,8 @@
+mod handlers;
+mod models;
+mod routers;
+mod state;
+
 fn main() {
     println!("Hello, world!");
 }
